681565738425eea9a9a0ef1d38a900c0c0c25644
Renan Araujo Polainas <re.araujo.po@gmail.com>
2025-07-29 22:37:09 -0300

    add: transform e desenho

--- actor.js
+++ actor.js
@@ -1,8 +1,38 @@
-class Actor{
-  constructor(){
-    this.machine = new Machine()
+class Actor {
+  constructor() {
+    // this.machine = new Machine();
+    this.transform = new Transform(200, 200, 0);
   }
-  
+
+  draw() {
+    const { position: pos } = this.transform;
+    push();
+    this.transform.applyTransform();
+    triangle(-15, 20, -15, -20, 10, 0);
+    pop();
+  }
 }
 
+class Transform {
+  constructor(x = 0, y = 0, rotation = 0) {
+    this.position = createVector(x, y);
+    this.rotation = rotation;
+  }
 
+  setPosition(x, y) {
+    this.position.set(x, y);
+  }
+
+  setRotation(angle) {
+    this.rotation = angle;
+  }
+
+  rotate(delta) {
+    this.rotation += delta;
+  }
+
+  applyTransform() {
+    translate(this.position.x, this.position.y);
+    rotate(this.rotation);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,32 +1,32 @@
+let a;
+
 function setup() {
   const canvas = createCanvas(400, 400);
-  setupUI(canvas)
+  a = new Actor();
 }
 
 function draw() {
   background(150);
+  a.draw();
 }
 
+function setupUI(mainFrame) {
+  const canvasDiv = createDiv();
+  canvasDiv.id("canvas-div");
+  mainFrame.parent(canvasDiv);
 
-function setupUI(mainFrame){
-  const canvasDiv = createDiv()
-  canvasDiv.id("canvas-div")
-  mainFrame.parent(canvasDiv)
-  
-  const menuDiv = createDiv()
-  menuDiv.id("menu")
-  menuDiv.position(width/3, height/2)
-  
-  const start = createButton("Começar")
-  start.mouseClicked(startGame)
-  start.id("comecar")
-  start.parent(menuDiv)
-  
-  const explic = createButton("Explicação")
-  explic.id("explic")
-  explic.parent(menuDiv)
+  const menuDiv = createDiv();
+  menuDiv.id("menu");
+  menuDiv.position(width / 3, height / 2);
+
+  const start = createButton("Começar");
+  start.mouseClicked(startGame);
+  start.id("comecar");
+  start.parent(menuDiv);
+
+  const explic = createButton("Explicação");
+  explic.id("explic");
+  explic.parent(menuDiv);
 }
 
-function startGame(){
-  
-}
+function startGame() {}
